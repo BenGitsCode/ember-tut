2e75e31ad20441d42e30663d4e239e24e7a94bdd
benjamski <benjadamski@gmail.com>
2016-08-14 16:19:13 -0400

    update model hook in route handler
    to access data store

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
+export default Ember.Route.extend({
+  model() {
+    return this.get('store').findAll('rental');
+  }
+});
+
 let rentals = [{
   id: 1,
   title: 'Grand Old Mansion',
